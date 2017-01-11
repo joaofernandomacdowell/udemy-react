18516140bf5229467ed2aa5a99e67100cd936416
João Fernando <joaofernando.costa@corp.globo.com>
2017-01-11 09:19:30 -0200

    Updates the file according to the first challenge

--- public/app.jsx
+++ public/app.jsx
@@ -16,18 +16,34 @@ var GreeterForm = React.createClass({
   onFormSubmit: function (e) {
     e.preventDefault();
 
+    var updates = {};
     var name = this.refs.name.value;
+    var message = this.refs.message.value;
 
     if (name.length > 0) {
       this.refs.name.value = '';
-      this.props.onNewName(name);
+      updates.name = name;
     }
+
+    if(message.length > 0) {
+      this.refs.message.value = '';
+      updates.message = message;
+    }
+
+    this.props.onNewData(updates);
   },
   render: function () {
     return (
       <form onSubmit={this.onFormSubmit}>
-        <input type="text" ref="name"/>
-        <button>Set Name</button>
+        <div>
+          <input type="text" ref="name"/>
+        </div>
+        <div>
+          <textarea ref="message" rows="2" cols="18" placeholder="Enter message"></textarea>
+        </div>
+        <div>
+          <button>Submit</button>
+        </div>
       </form>
     );
   }
@@ -42,31 +58,31 @@ var Greeter = React.createClass({
   },
   getInitialState: function () {
     return {
-      name: this.props.name
+      name: this.props.name,
+      message: this.props.message
     };
   },
-  handleNewName: function (name) {
-    this.setState({
-      name: name
-    });
+  handleNewData: function (updates) {
+    this.setState(updates);
   },
+
   render: function () {
     var name = this.state.name;
-    var message = this.props.message;
+    var message = this.state.message;
 
     return (
       <div>
         <GreeterMessage name={name} message={message}/>
-        <GreeterForm onNewName={this.handleNewName}/>
+        <GreeterForm onNewData={this.handleNewData}/>
       </div>
 
     );
   }
 });
 
-var name = 'João';
+var firstName = 'João';
 
 ReactDOM.render(
-  <Greeter name={name}/>,
+  <Greeter name={firstName}/>,
   document.getElementById('app')
 );
